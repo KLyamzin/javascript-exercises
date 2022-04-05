fd75127898d5a6c1dc8509796144a54116cb8056
Kirill Lyamzin <kiryanoff88@gmail.com>
2022-04-05 17:00:00 -0400

    resolved homework

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,11 @@
-const repeatString = function() {
-
+const repeatString = function (string, int) {
+    return string.repeat(int); // 1 line
+    // using for loop
+    // let holder = '';
+    // for (let i = 0; i < int; i++) {
+    //     holder += string;
+    // };
+    // return holder;
 };
 
 // Do not edit below this line
