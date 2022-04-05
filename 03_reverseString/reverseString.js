364f93ad40fa8cc726fe424a10ed736e1b2a1ebe
Kirill Lyamzin <kiryanoff88@gmail.com>
2022-04-05 17:52:12 -0400

    resolved homework

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,6 @@
-const reverseString = function() {
-
+const reverseString = function (string) {
+    let reverseString = [...string].reverse().join('');
+    return reverseString;
 };
 
 // Do not edit below this line
