5a3d2983a09b8e08e778096429e2fd6eb0103917
Kirill Lyamzin <kiryanoff88@gmail.com>
2022-04-06 11:42:49 -0400

    completed the assigment

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,6 +1,8 @@
-const removeFromArray = function() {
-
+const removeFromArray = function (importedArray, ...toRemoveArguments) {
+    const finalArray = importedArray.filter(item => !toRemoveArguments.includes(item));
+    return finalArray;
 };
+
 
 // Do not edit below this line
 module.exports = removeFromArray;
