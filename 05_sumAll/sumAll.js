c4c46dc7b85fde0916bca0f52047bada91b2973c
Kirill Lyamzin <kiryanoff88@gmail.com>
2022-04-06 14:57:23 -0400

    problem solved

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,21 @@
-const sumAll = function() {
-
+const sumAll = function (firstNum, lastNum) {
+    // returning 'ERROR' if not a number; less that 0; is a string
+    if (isNaN(firstNum) || isNaN(lastNum)) return 'ERROR';
+    if (typeof (firstNum) == "string" || typeof (lastNum) == "string") return 'ERROR';
+    if (firstNum < 0 || lastNum < 0) return 'ERROR';
+    // sorting from smallest to biggest
+    let min = Math.min(firstNum, lastNum)
+    let max = Math.max(firstNum, lastNum)
+    // using a for loop
+    let sum = 0;
+    for (let j = min; j <= max; j++) {
+        sum = sum + j;
+    }
+    return sum;
+    //using an algorithm
+    /*    let num = (max - min + 1);
+       let sumNum = num * (min + max) / 2;
+       return sumNum */
 };
 
 // Do not edit below this line
