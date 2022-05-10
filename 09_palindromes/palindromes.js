06eb8c8b21a3ae3f0e5d024329560b33333c9f77
Kirill Lyamzin <kiryanoff88@gmail.com>
2022-05-10 17:03:48 -0400

    Palindromes is complete

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,6 +1,8 @@
-const palindromes = function () {
-
-};
-
+const palindromes = function (string) {
+    const re = /[\W]/g;
+    const lowOriginal = string.toLowerCase().replace(re, '');
+    const lowReversed = lowOriginal.split('').reverse().join('');
+    return lowReversed === lowOriginal;
+}
 // Do not edit below this line
 module.exports = palindromes;
