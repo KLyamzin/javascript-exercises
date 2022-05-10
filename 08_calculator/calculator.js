72ad9d59ac355db6dbf29882ae5383766987edb5
Kirill Lyamzin <kiryanoff88@gmail.com>
2022-05-10 15:39:27 -0400

    Calculator is complete

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,29 @@
-const add = function() {
-	
+const add = function (a, b) {
+  return a + b;
 };
 
-const subtract = function() {
-	
+const subtract = function (a, b) {
+  return a - b;
 };
 
-const sum = function() {
-	
+const sum = function (array) {
+  let summ = array.reduce((a, b) => a + b, 0)
+  return summ;
 };
 
-const multiply = function() {
-
+const multiply = function (array) {
+  let summ = array.reduce((a, b) => a * b)
+  return summ;
 };
 
-const power = function() {
-	
+const power = function (a, b) {
+  return Math.pow(a, b);
 };
 
-const factorial = function() {
-	
+const factorial = function (a) {
+  let b = 1;
+  if (a == 0 || a == 1) return 1;
+  else return a * factorial(a - 1);
 };
 
 // Do not edit below this line
