25ad8eeb4f85c10c87788c152cb8ed882fd82016
Kirill Lyamzin <kiryanoff88@gmail.com>
2022-05-10 23:14:06 -0400

    findTheOldest is  complete

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,6 +1,18 @@
-const findTheOldest = function() {
-
+const findTheOldest = function (person) {
+    return person.reduce((oldest, current) => {
+        const oldestAge = setAge(oldest.yearOfBirth, oldest.yearOfDeath);
+        const currentAge = setAge(current.yearOfBirth, current.yearOfDeath)
+        return oldestAge < currentAge ? current : oldest;
+    });
 };
+const setAge = function (birth, death) {
+    if (!death) {
+        death = new Date().getFullYear();
+    }
+    return death - birth;
+}
+
+
 
 // Do not edit below this line
 module.exports = findTheOldest;
